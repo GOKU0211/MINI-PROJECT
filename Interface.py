731dc33b372336a6ae363e7b592d831c09ee5892
GOKU0211 <nipunhavok09@gmail.com>
2025-10-13 01:09:00 +0530

    made a few changes added a notes.txt

--- Interface.py
+++ Interface.py
@@ -2,6 +2,8 @@
 import customtkinter as ctk
 from PIL import Image
 import voice_controlled as vc
+import threading
+
 ctk.set_appearance_mode("dark")
 
 main=ctk.CTk()
@@ -14,6 +16,11 @@ img=ctk.CTkImage(light_image=Image.open(r"D:/MINI PROJECT/main mini project/MINI
 img_label = ctk.CTkLabel(main, image=img, text="")
 img_label.pack(pady=250)
 
+# Label to show text output
+output_label = ctk.CTkLabel(main, text="Click the mic to start listening", font=("Arial", 16))
+output_label.pack(pady=200)
+
+
 #making the image a button
 def mic_button():
     for scale in [0.9, 1.0]:  # shrink and restore
@@ -22,11 +29,16 @@ def mic_button():
         img_label.configure(image=new_img)
         img_label.image = new_img
         main.update()
-        main.after(80)
+        main.after(100)
+
+#funtion to start listening
+def start_listening_thread():
+    threading.Thread(target=start_listening, daemon=True).start()
 
 #on clicking 
 def on_click(event=None):
     mic_button()
+    output_label.configure(text="🎙️ Listening... Please speak now.")
     vc.listen_command()
 
 #binding the click
